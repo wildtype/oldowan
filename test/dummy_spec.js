253e74fe3e2734b17b18d03758c168c6c0c032f8
Mufti <iwak.sotong@gmail.com>
2018-02-25 16:18:05 +0700

    Use spec reporter

---
+++ test/dummy_spec.js
@@ -0,0 +1,9 @@
+describe('Somme dummy test', () => {
+  it('Pass when true', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('Also pass when false', () => {
+    expect(false).toBeFalsy();
+  });
+});
