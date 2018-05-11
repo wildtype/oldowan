97bbf0c8b65e3257c7488bbe435e330c39366195
Mufti <iwak.sotong@gmail.com>
2018-05-12 02:31:21 +0700

    Add SASS

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,38 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    './src/index.js',
+    './src/stylesheets/application.scss'
+  ],
+
   output: {
-    filename: 'bundle.js',
+    filename: 'application.js',
     path: path.resolve(__dirname, 'dist')
   },
+
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
       }
-    }]
-  }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'stylesheets/application.css',
+      allChunks: true
+    })
+  ]
 };
